d0eb8dc12ccee52524511949b09aa3e5fcbfc247
unknown <pe_gasa@hotmail.com>
2021-09-02 16:45:24 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 80106376,
+    "boundingBox": {
+        "lx": 478062.8699373101,
+        "ly": 2139430.4738925385,
+        "lz": 2293.3371357920187,
+        "ux": 478953.2619417176,
+        "uy": 2140320.865896946,
+        "uz": 3183.7291401994953
+    },
+    "tightBoundingBox": {
+        "lx": 478062.8699350938,
+        "ly": 2139430.473894742,
+        "lz": 2293.3371673062197,
+        "ux": 478449.47933509378,
+        "uy": 2140320.865894742,
+        "uz": 2396.51176730622
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 7.7110209465026859,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
